bf81557b37cba892f1acf2eba04e6311bbe0793f
bashir mahmoudi <bashir.mahmoudi@gmail.com>
2018-04-20 23:31:18 +0430

    Project Structure

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, ViewEncapsulation } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,10 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+document.addEventListener('deviceready', () => {
+  platformBrowserDynamic().bootstrapModule(AppModule, {
+    defaultEncapsulation: ViewEncapsulation.None
+  }).catch(err => console.log(err));
+}, false);
+
+
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,11 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+interface JQuery {
+  niceScroll(options?: {
+    cursorcolor: string,
+    cursorwidth: number,
+    cursorborder: string
+  }): any;
+}
